152647209ef26d1428cc73fce02fc4a962719cbf
Rico.Chen <rico_dd@163.com>
2017-02-28 16:10:58 +0800

    add Domain User

---
+++ src/main/java/rico/demo/domain/User.java
@@ -0,0 +1,15 @@
+package rico.demo.domain;
+
+import lombok.Data;
+
+/**
+ * Created by Rico.Chen on 2017/2/28.
+ */
+@Data
+public class User {
+
+    private String loginName;
+
+    private String loginPassword;
+
+}
